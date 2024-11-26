ac389597822e7a8adbdef497d939faa71b9c4522
Md Mushfiqur Rahman <mushfiq2000cpt@gmail.com>
2024-11-26 22:02:08 +0600

    Get single and all courses --without purchasing, course content, user accessible all courses --only for valid user

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -4,7 +4,7 @@ import ErrorHandler from "../utils/ErrorHandler";
 import cloudinary from "cloudinary";
 import { createCourse } from "../services/course.service";
 import CourseModel from "../models/course.model";
-
+import { redis } from "../utils/redis";
 
 
 // upload course
@@ -21,7 +21,7 @@ export const uploadCourse = CatchAsyncError(
 				data.thumbnail = {
 					public_id: myCloud.public_id,
 					url: myCloud.secure_url,
-				}
+				};
 			}
 			createCourse(data, res, next);
 		} catch (error: any) {
@@ -32,41 +32,139 @@ export const uploadCourse = CatchAsyncError(
 
 // edit course
 export const editCourse = CatchAsyncError(
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const data = req.body;
+
+			const thumbnail = data.thumbnail;
+
+			// const courseId = req.params.id;
+
+			// const courseData = await CourseModel.findById(courseId) as any;
+
+			if (thumbnail) {
+				await cloudinary.v2.uploader.destroy(thumbnail.public_id);
+
+				const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+					folder: "courses",
+				});
+
+				data.thumbnail = {
+					public_id: myCloud.public_id,
+					url: myCloud.secure_url,
+				};
+			}
+
+			const courseId = req.params.id;
+			const course = await CourseModel.findByIdAndUpdate(
+				courseId,
+				{
+					$set: data,
+				},
+				{ new: true }
+			);
+
+			res.status(201).json({
+				success: true,
+				course,
+			});
+		} catch (error: any) {
+			return next(new ErrorHandler(error.message, 500));
+		}
+	}
+);
+
+// get single course --- without purchasing
+export const getSingleCourse = CatchAsyncError(
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const courseId = req.params.id;
+
+			const isCacheExist = await redis.get(courseId);
+
+			if (isCacheExist) {
+				const course = JSON.parse(isCacheExist);
+				res.status(200).json({
+					success: true,
+					course,
+				});
+			} else {
+				const course = await CourseModel.findById(req.params.id).select(
+					"-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+				);
+
+				await redis.set(courseId, JSON.stringify(course), "EX", 604800); // 7days
+
+				res.status(200).json({
+					success: true,
+					course,
+				});
+			}
+		} catch (error: any) {
+			return next(new ErrorHandler(error.message, 500));
+		}
+	}
+);
+
+// get all courses --- without purchasing
+export const getAllCourses = CatchAsyncError(
+	async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const isCacheExist = await redis.get("allCourses");
+			if (isCacheExist) {
+				const courses = JSON.parse(isCacheExist);
+				console.log('hitting redis');
+				res.status(200).json({
+					success: true,
+					courses,
+				});
+			} else {
+				const courses = await CourseModel.find().select(
+					"-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+				);
+
+				console.log('hitting mongodb');
+
+				await redis.set("allCourses", JSON.stringify(courses));
+
+				res.status(200).json({
+					success: true,
+					courses,
+				});
+			}
+		} catch (error: any) {
+			return next(new ErrorHandler(error.message, 500));
+		}
+	}
+);
+
+
+// get course content -- only for valid user
+export const getCourseByUser = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const data = req.body;
+      const userCourseList = req.user?.courses;
+		const courseId = req.params.id;
+		
+		console.log(courseId);
 
-      const thumbnail = data.thumbnail;
-
-     // const courseId = req.params.id;
-
-     // const courseData = await CourseModel.findById(courseId) as any;
-
-      if (thumbnail) {
-        await cloudinary.v2.uploader.destroy(thumbnail.public_id);
-
-        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
-          folder: "courses",
-        });
-
-        data.thumbnail = {
-          public_id: myCloud.public_id,
-          url: myCloud.secure_url,
-        };
-      }
-
-        const courseId = req.params.id;
-        const course = await CourseModel.findByIdAndUpdate(
-        courseId,
-        {
-          $set: data,
-        },
-        { new: true }
+      const courseExists = userCourseList?.find(
+        (course: any) => course._id.toString() === courseId
       );
 
-      res.status(201).json({
+      if (!courseExists) {
+        return next(
+          new ErrorHandler("You are not eligible to access this course", 404)
+        );
+      }
+
+      const course = await CourseModel.findById(courseId);
+
+      const content = course?.courseData;
+
+      res.status(200).json({
         success: true,
-        course,
+        content,
       });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 500));

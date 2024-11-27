08db5669eeab8c45dceedf1896ac589a183758e5
Md Mushfiqur Rahman <mushfiq2000cpt@gmail.com>
2024-11-28 01:15:38 +0600

    Get all notifications, update notification status

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -9,6 +9,7 @@ import mongoose from "mongoose";
 import path from "path";
 import ejs from "ejs";
 import sendMail from "../utils/sendMails";
+import NotificationModel from "../models/notification.Model";
 //import NotificationModel from "../models/notification.Model";
 
 // upload course
@@ -211,6 +212,12 @@ export const addQuestion = CatchAsyncError(
 			// add this question to our course content
 			courseContent.questions.push(newQuestion);
 
+			await NotificationModel.create({
+				user: req.user?._id,
+				title: "New Question Received",
+				message: `You have a new question in ${courseContent.title}`,
+			});
+
 			// save the updated course
 			await course?.save();
 
@@ -280,11 +287,11 @@ export const addAnswer = CatchAsyncError(
 
 			if (req.user?._id === question.user._id) {
 				// create a notification
-				// await NotificationModel.create({
-				//user: req.user?._id,
-				//title: "New Question Reply Received",
-				//message: `You have a new question reply in ${courseContent.title}`,
-				//});
+				await NotificationModel.create({
+					user: req.user?._id,
+					title: "New Question Reply Received",
+					message: `You have a new question reply in ${courseContent.title}`,
+				});
 			} else {
 				const data = {
 					name: question.user.name,
